60e60558e5a896aaf7401178f978051c7da4380d
Maria Jordana <jordana-cavalcante@outlook.com>
2022-03-28 19:13:21 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+
+        Position pos = new Position(1,3);
+        System.out.println(pos);
     }
 }
